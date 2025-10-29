429e03c6871fce9ba34357799d8708900d53da41
Hannah <baixinhamanes@hotmail.com>
2025-10-28 21:07:37 -0300

    final do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,84 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  // Char para caracteres e strings, estes dispostos em um array, para o nome da cidade e códigos da carta e do estado
+  char estado, estado2, nome[50], nome2[50], codigo[10], codigo2[10];
+  // números inteiros para representar a população e pontos turísticos
+  int populacao, populacao2, pontos_turisticos, pontos_turisticos2;
+  // números decimais para a área e o pib
+  float area, area2, pib, pib2;
+
   // Área para entrada de dados
+
+  // Informações sobre a carta 1 
+  
+  printf("Informações sobre a carta 1: \n"); // printf printa as informações e instruções para criação das cartas na tela do terminal
+  printf("Insira uma letra de A a H para representar o estado: "); // scanf permite ao usuário inserir as informações desejadas pelo terminal
+  scanf(" %c", &estado);
+
+  printf("Insira o código da carta (a letra escolhida para o estado + um número de 01 a 04. Ex: A01, H04): ");
+  scanf("%s", codigo);
+
+  printf("Insira o nome da cidade: ");
+  scanf("%s", nome);
+
+  printf("Insira o número aproximado de habitantes da cidade (sem pontuação): ");
+  scanf("%d", &populacao);
+
+  printf("Insira a área da cidade em quilômetros quadrados (em números decimais, se necessário): ");
+  scanf("%f", &area);
+
+  printf("Insira o PIB (Produto Interno Bruto) da cidade (em números decimais, se necessário. Não é necessário adicionar pontos ou zeros (000) para a casa dos milhões e bilhões): ");
+  scanf("%f", &pib);
+
+  printf("Insira o número de pontos turísticos presentes na cidade: ");
+  scanf("%d", &pontos_turisticos);
+
+  // Informações sobre a carta 2
+
+  printf("\nInformações sobre a carta 2: \n");
+  printf("Insira uma letra de A a H para representar o estado: ");
+  scanf(" %c", &estado2);
+
+  printf("Insira o código da carta (a letra escolhida para o estado + um número de 01 a 04. Ex: A01, H04): ");
+  scanf("%s", codigo2);
+
+  printf("Insira o nome da cidade: ");
+  scanf("%s", nome2);
+
+  printf("Insira o número aproximado de habitantes da cidade (sem pontuação): ");
+  scanf("%d", &populacao2);
+
+  printf("Insira a área da cidade em quilômetros quadrados (em números decimais, se necessário): ");
+  scanf("%f", &area2);
+
+  printf("Insira o PIB (Produto Interno Bruto) da cidade (em números decimais, se necessário. Não é necessário adicionar pontos ou zeros (000) para a casa dos milhões e bilhões): ");
+  scanf("%f", &pib2);
+
+  printf("Insira o número de pontos turísticos presentes na cidade: ");
+  scanf("%d", &pontos_turisticos2);
 
   // Área para exibição dos dados da cidade
 
-return 0;
+  // Informações sobre a carta 1
+  printf("\nCarta 1: \n");
+  printf("Estado: %c\n", estado);
+  printf("Codigo: %s\n", codigo);
+  printf("Nome da Cidade: %s\n", nome);
+  printf("População: %d\n", populacao);
+  printf("Área: %.2f km²\n", area);
+  printf("PIB: %.2f bilhões de reais\n", pib);
+  printf("Número de Pontos Turísticos: %d\n\n", pontos_turisticos);
+  
+  // Informações sobre a carta 2
+  printf("Carta 2: \n");
+  printf("Estado: %c\n", estado2);
+  printf("Codigo: %s\n", codigo2);
+  printf("Nome da Cidade: %s\n", nome2);
+  printf("População: %d\n", populacao2);
+  printf("Área: %.2f km²\n", area2);
+  printf("PIB: %.2f bilhões de reais\n", pib2);
+  printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+
+  return 0;
 } 
